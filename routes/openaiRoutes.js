b4d3d479326e8f210185837ecf876d382b3c9163
Joey Cadieux <instructor.joeycadieux@gmail.com>
2025-02-12 01:09:04 +0000

    Adding some files...

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const {generateImage} = require('../controllers/openaiController');
+const { generateImage } = require('../controllers/openaiController');
 const router = express.Router();
 
 
-router.post('/generateimage', generateImage => {
-    res.status(200).json({
-        success: true,
-    });
-});
+
+router.post('/generateimage', generateImage);
+
 
 module.exports = router;
